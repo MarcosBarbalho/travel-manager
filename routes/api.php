11756c52113f78a7012ebe63bbc997a883358fb9
Marcos Barbalho <marcos.barbalho.w@gmail.com>
2024-11-07 23:19:11 -0300

    feat: adiciona rotas de autenticacao

--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,28 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\PingController;
+use App\Http\Controllers\UserController;
 use Illuminate\Routing\Router;
 
 /* @var $router Router */
 
 $loginLimiter = config('app.rate_limit.login');
 
-$router->middleware('guest')->group(function (Router $router) use ($loginLimiter) {
-    $router->get('/ping', [PingController::class, 'show'])->name('ping.show');
+$router->get('/ping', [PingController::class, 'show'])->name('ping');
 
+$router->middleware('guest')->group(function (Router $router) use ($loginLimiter) {
     $router->middleware(["throttle:{$loginLimiter},1"])->group(function (Router $router) {
+        $router->post('/login', [AuthenticationController::class, 'login'])->name('login');
+
+        $router->post('/register', [AuthenticationController::class, 'register'])->name('register');
+    });
+});
+
+$router->middleware('auth:jwt')->group(function (Router $router) {
+    $router->get('/logout', [AuthenticationController::class, 'logout'])->name('logout');
+
+    $router->group(['prefix' => '/me'], function (Router $router) {
+        $router->get('/', [UserController::class, 'show'])->name('me.user');
     });
 });
