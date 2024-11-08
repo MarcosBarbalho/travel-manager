1341ed77a1af258bef259a92f4fd20806f2447ef
Marcos Barbalho <marcos.barbalho.w@gmail.com>
2024-11-08 00:01:50 -0300

    refactor: atualiza BaseTestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,23 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Foundation\Testing\WithFaker;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    use LazilyRefreshDatabase;
+    use WithFaker;
+
+    public function actingAs($user, $guard = null)
+    {
+        $token = auth()->login($user);
+
+        $this->withHeader('Authorization', "Bearer {$token}");
+
+        parent::actingAs($user);
+
+        return $this;
+    }
 }
